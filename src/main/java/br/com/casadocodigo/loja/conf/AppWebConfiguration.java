72249c24d4f5b9218a7d5f1037adbf0c4fb44705
Gabriel Ferreira <gabriel@jostle.me>
2018-06-13 23:21:02 -0700

    allowed Crping to access the resources folder and added the CSS files

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -13,12 +13,14 @@ import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @ComponentScan(basePackageClasses = {HomeController.class, ProdutoDAO.class, FileSaver.class})
-public class AppWebConfiguration {
+public class AppWebConfiguration implements WebMvcConfigurer {
 
     @Bean   // Todas as classes gerenciadas pelo Spring tem essa anotacao Bean(semente)
     public InternalResourceViewResolver internalResourceViewResolver() {
@@ -56,4 +58,10 @@ public class AppWebConfiguration {
         return new StandardServletMultipartResolver();
     }
 
+    // este método faz com que o Spring libere o acesso a pasta resources, onde está contido o CSS. Para que este método
+    // funicone, devemmos implementar WebMvcConfigurer
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
